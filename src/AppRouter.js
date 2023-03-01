8bf8654d4d726bf5b389d8cadd25667742b8cfd2
Melanie Mai <melaniesmai@outlook.com>
2023-03-01 16:57:10 -0500

    update route

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { HashRouter } from "react-router-dom";
+
 import Work from "./components/Content/Work";
 import About from "./components/About";
 
 function AppRouter() {
   return (
-    <Router basename={process.env.PUBLIC_URL + "/"}>
+    <HashRouter>
       <Routes>
         <Route path="/" exact element={<Work />} />
         <Route path="/about" exact element={<About />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
